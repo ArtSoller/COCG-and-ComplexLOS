1967e5dc263fff3762dff1a4350588f6e1c7e7f3
Artyom <art-sav2002@yandex.ru>
2025-11-07 14:38:36 +0700

    Create and add folders

---
+++ SLAE_Tests/Main.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SLAETests
+{
+	TEST_CLASS(SLAETests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
